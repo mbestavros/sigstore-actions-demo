92280cf8174b22b5f3740285fca5b601a31e57e2
Mark Bestavros <mbestavr@redhat.com>
2022-11-15 23:19:27 +0300

    refactor demo to use separate images
    Signed-off-by: Mark Bestavros <mbestavr@redhat.com>

--- demo.sh
+++ demo.sh
@@ -9,37 +9,15 @@ DEMO_PROMPT="${GREEN}➜ ${CYAN}\W "
 
 clear
 
-pei "# set log_id from GitHub Action"
-
-cmd
-
-pe "rekor-cli get --log-index=$log_id --format=json | jq \".Body | .HashedRekordObj | .signature | .publicKey | .content\" | cut -d '\"' -f2 | base64 -d > cert-action.pem"
-
-pe "openssl x509 -in cert-action.pem -text -noout"
-
-pe 'tail -n 28 manifests/imagePolicy.yaml'
-
-pe "kubectl apply -f manifests/imagePolicy.yaml --namespace next-demo"
-
-pe "kubectl run bad-image --image=ghcr.io/lukehinds/blackhat-next-security-demo:main"
-
-pe "kubectl run nginx --image=nginx:latest"
-
-pe "kubectl run good-image --image=ghcr.io/mbestavros/redhat-day-security-demo:main"
-
-pe "kubectl get pod good-image"
-
-pe "kubectl delete pod good-image"
-
 pe "tail -n 30 manifests/imagePolicy-email.yaml"
 
 pe "kubectl apply -f manifests/imagePolicy-email.yaml --namespace next-demo"
 
-pe "kubectl run good-image --image=ghcr.io/mbestavros/redhat-day-security-demo:main"
+pe "kubectl run nginx --image=nginx:latest"
 
-pe "cosign sign ghcr.io/mbestavros/redhat-day-security-demo:main"
+pe "cosign sign ghcr.io/mbestavros/redhat-day-security-demo-unsigned:main"
 
-p "# log_id from email"
+p "# set log_id from cosign"
 
 cmd
 
@@ -47,6 +25,30 @@ pe "rekor-cli get --log-index=$log_id --format=json | jq \".Body | .HashedRekord
 
 pe "openssl x509 -in cert-email.pem -text -noout"
 
+pe "kubectl run good-image --image=ghcr.io/mbestavros/redhat-day-security-demo-unsigned:main"
+
+pe "kubectl get pod good-image"
+
+pe "kubectl delete pod good-image"
+
+p "# Over to Github!"
+
+p "# set log_id from GitHub Action"
+
+cmd
+
+pe "rekor-cli get --log-index=$log_id --format=json | jq \".Body | .HashedRekordObj | .signature | .publicKey | .content\" | cut -d '\"' -f2 | base64 -d > cert-action.pem"
+
+pe "openssl x509 -in cert-action.pem -text -noout"
+
+pe 'tail -n 28 manifests/imagePolicy-ephemeral.yaml'
+
+pe "kubectl apply -f manifests/imagePolicy-ephemeral.yaml --namespace next-demo"
+
+pe "kubectl run bad-image --image=ghcr.io/lukehinds/blackhat-next-security-demo:main"
+
 pe "kubectl run good-image --image=ghcr.io/mbestavros/redhat-day-security-demo:main"
+
+pe "kubectl get pod good-image"
 
 pe "kubectl delete pod good-image"
